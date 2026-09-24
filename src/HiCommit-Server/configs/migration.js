46f33264f5668bf56178026859516d3387d9ffad
ngocdanthanhtvu <103914827+ngocdanthanhtvu@users.noreply.github.com>
2026-09-24 04:32:15 +0000

    Separate database migration credentials

--- src/HiCommit-Server/configs/migration.js
+++ src/HiCommit-Server/configs/migration.js
@@ -2,15 +2,15 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    username: process.env.MIGRATION_DB_USER || process.env.DB_USER,
+    password: process.env.MIGRATION_DB_PASSWORD || process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'mysql'
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    username: process.env.MIGRATION_DB_USER || process.env.DB_USER,
+    password: process.env.MIGRATION_DB_PASSWORD || process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'mysql'
